2579dc80c50159ae94d70d01df690cbf2899360d
uuuusssseeeerrrr <kyozzang20@gmail.com>
2025-10-13 21:52:39 +0900

    security 수정

--- src/main/kotlin/com/ietf/etfbatch/BatchApiController.kt
+++ src/main/kotlin/com/ietf/etfbatch/BatchApiController.kt
@@ -4,6 +4,7 @@ import com.ietf.etfbatch.stock.service.KisInfoService
 import com.ietf.etfbatch.stock.service.KisStockService
 import com.ietf.etfbatch.token.service.KisTokenService
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
@@ -15,56 +16,58 @@ fun Application.configureRouting() {
     val kisStockService by inject<KisStockService>()
 
     routing {
-        get("/token") {
-            try {
-                kisTokenService.getKisAccessToken()
-            } catch (e: Exception) {
-                logger.error(e.message, e)
-                call.respondText(
-                    "token 요청중 오류발생"
-                )
+        authenticate("tokenAuth") {
+            get("/token") {
+                try {
+                    kisTokenService.getKisAccessToken()
+                } catch (e: Exception) {
+                    logger.error(e.message, e)
+                    call.respondText(
+                        "token 요청중 오류발생"
+                    )
+                }
+
+                call.respondText("token 요청 처리됨")
             }
 
-            call.respondText("token 요청 처리됨")
-        }
+            get("/etf") {
+                try {
+                    kisStockService.getEtfPrice()
+                } catch (e: Exception) {
+                    logger.error(e.message, e)
+                    call.respondText(
+                        "etf 요청중 오류발생"
+                    )
+                }
 
-        get("/etf") {
-            try {
-                kisStockService.getEtfPrice()
-            } catch (e: Exception) {
-                logger.error(e.message, e)
-                call.respondText(
-                    "etf 요청중 오류발생"
-                )
+                call.respondText("etf 요청 처리됨")
             }
 
-            call.respondText("etf 요청 처리됨")
-        }
+            get("/stock") {
+                try {
+                    kisStockService.getStockPrice()
+                } catch (e: Exception) {
+                    logger.error(e.message, e)
+                    call.respondText(
+                        "stock 요청중 오류발생"
+                    )
+                }
 
-        get("/stock") {
-            try {
-                kisStockService.getStockPrice()
-            } catch (e: Exception) {
-                logger.error(e.message, e)
-                call.respondText(
-                    "stock 요청중 오류발생"
-                )
+                call.respondText("stock 요청 처리됨")
             }
 
-            call.respondText("stock 요청 처리됨")
-        }
+            get("/kisInfo") {
+                try {
+                    kisInfoService.kisInfo()
+                } catch (e: Exception) {
+                    logger.error(e.message, e)
+                    call.respondText(
+                        "kisInfo 요청중 오류발생"
+                    )
+                }
 
-        get("/kisInfo") {
-            try {
-                kisInfoService.kisInfo()
-            } catch (e: Exception) {
-                logger.error(e.message, e)
-                call.respondText(
-                    "kisInfo 요청중 오류발생"
-                )
+                call.respondText("kisInfo 요청 처리됨")
             }
-
-            call.respondText("kisInfo 요청 처리됨")
         }
     }
 }
--- src/main/kotlin/com/ietf/etfbatch/config/SecurityConfig.kt
+++ src/main/kotlin/com/ietf/etfbatch/config/SecurityConfig.kt
@@ -1,19 +1,16 @@
 package com.ietf.etfbatch.config
 
-import io.ktor.server.application.Application
-import io.ktor.server.application.install
-import io.ktor.server.auth.Authentication
-import io.ktor.server.auth.UserIdPrincipal
-import io.ktor.server.auth.bearer
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
 
 fun Application.configureSecurity() {
     val token = environment.config.property("custom.batchToken").getString()
 
     install(Authentication) {
-        bearer {
+        bearer("tokenAuth") {
             authenticate { tokenCredential ->
                 if (tokenCredential.token == token) {
-                    UserIdPrincipal("auth")
+                    "ok"
                 } else {
                     null
                 }
