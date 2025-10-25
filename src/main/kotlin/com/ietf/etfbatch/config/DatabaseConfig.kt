0e532545534bb831d362735bccd9275434657bd9
uuuusssseeeerrrr <kyozzang20@gmail.com>
2025-10-25 20:53:39 +0900

    데이터베이스 url관련 수정

--- src/main/kotlin/com/ietf/etfbatch/config/DatabaseConfig.kt
+++ src/main/kotlin/com/ietf/etfbatch/config/DatabaseConfig.kt
@@ -6,9 +6,12 @@ import org.jetbrains.exposed.v1.jdbc.transactions.TransactionManager
 
 object DataSourceFactory {
     fun init() {
+        val isDocker = System.getenv("DOCKER_ENV")?.toBoolean() ?: false
+        val dbUrlKey = if (isDocker) "DB_URL_DOCKER" else "DB_URL"
+
         val db = Database.connect(
             driver = "org.mariadb.jdbc.Driver",
-            url = "${VaultConfig.getVaultSecret("db_url")}?rewriteBatchedStatements=true",
+            url = "${VaultConfig.getVaultSecret(dbUrlKey)}?rewriteBatchedStatements=true",
             user = "etf",
             password = VaultConfig.getVaultSecret("db_password"),
             databaseConfig = DatabaseConfig {
